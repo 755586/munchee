99d2657c82a48d147d34a5178e9a2c07277abdda
const_Q <755545586@qq.com>
2019-01-27 14:59:50 +0800

    first spring boot

---
+++ src/main/java/com/qiu/munchee/MuncheeApplication.java
@@ -0,0 +1,14 @@
+package com.qiu.munchee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuncheeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuncheeApplication.class, args);
+    }
+
+}
+
